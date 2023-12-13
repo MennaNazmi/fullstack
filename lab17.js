018573400331d4ee0ef75fbfc79f459c553ae5ad
M <mennanazmii@gmail.com>
2023-12-13 09:15:11 +0200

    play with mousehover

--- lab17.js
+++ lab17.js
@@ -1,5 +1,23 @@
 var listvar=document.querySelector("#names")
-
+var b=document.querySelector("button")
 listvar.addEventListener("mouseover",function(){
-      document.body.classList.add("changecolorclass")
+    //   document.body.classList.add("changecolorclass")
+    document.body.style.color="green"
+})
+
+listvar.addEventListener("mouseout",function(){
+    // document.body.classList.remove("changecolorclass")
+    document.body.style.color="black"
+})
+
+
+b.addEventListener("mouseenter",function(){
+   b.style.visibility="hidden"
+})
+
+b.addEventListener("mouseleave ",function(){
+    // document.body.classList.remove("changecolorclass")
+    // document.body.style.color="black"
+    b.style.visibility="visible"
+   
 })
