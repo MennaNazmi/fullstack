0c7c9af916e31a8b0f8cbaecc7c3f603d5284566
M <mennanazmii@gmail.com>
2023-12-14 08:31:44 +0200

    color game

--- lab18.js
+++ lab18.js
@@ -1,15 +1,85 @@
-var colors =[
-    "rgb(255,0,0)",
-    "rgb(255,255,0)",
-    "rgb(0,255,0)",
-    "rgb(0,255,255)",
-    "rgb(0,0,255)",
-    "rgb(255,0,255)",
-]
+var colors = generateRandomColors(6);
 
-var squares=document.querySelectorAll(".square")
+
+
+// [ //there is a space in second and third colors
+//     "rgb(255, 0, 0)",
+//     "rgb(255, 255, 0)",
+//     "rgb(0, 255, 0)",
+//     "rgb(0, 255, 255)",
+//     "rgb(0, 0, 255)",
+//     "rgb(255, 0, 255)",
+// ]
+
+var squares=document.querySelectorAll(".square");
+var pickedcolor= pickColor() //colors[3];  
+var pickedcolordisplay=document.querySelector("#pickedcolor");
+var messageDisplay=document.querySelector("#message")
+var h1display=document.querySelector("h1")
+
 
 for(var i=0;i<squares.length;i++)
 {
-    console.log(squares[i].colors);
+    
+    squares[i].style.background = colors[i]
+      //console.log(squares[i].style.background = colors[i]);
+      ///
+      squares[i].addEventListener("click",function(){
+         //we clicked on this
+         var clickedColor =this.style.background
+       
+         console.log(clickedColor)
+         console.log(pickedcolor)
+         if(clickedColor===pickedcolor)
+         {
+           
+            messageDisplay.textContent="Correct"
+            h1display.style.backgroundColor=pickedcolor
+            changeColor(pickedcolor)
+         }
+         else {
+            //alert("wrong")
+            this.style.background="rgb(128, 128, 128)"
+            messageDisplay.textContent="Wrong"
+         }
+    })
+}
+pickedcolordisplay.textContent=pickedcolor;
+
+
+function changeColor(color)
+{
+    for(var i=0;i<squares.length;i++)
+    {
+        squares[i].style.background=color;
+    }
+
+}
+function pickColor(){
+   var random= Math.floor(Math.random()*colors.length);//1-5
+   return colors[random];//random in array
+}
+
+function generateRandomColors(num){
+    //console.log("generateRandomColors "+num)
+    var arr=[];
+    for (var i=0;i<num;i++)
+    {
+      
+        //console.log(ranColor())
+        arr.push(ranColor())
+        //arr[i]=randomcolor()
+    }
+  
+    return arr;
+}
+
+function ranColor()
+{
+    
+   var r= Math.floor(Math.random() * 256);
+   var g= Math.floor(Math.random() * 256);
+   var b= Math.floor(Math.random() * 256);
+   //console.log("rgb("+r+", "+g+", "+b+")");
+   return  "rgb("+r+", "+g+", "+b+")" ;
 }
